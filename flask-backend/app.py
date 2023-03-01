9a44b4e99ac1544cb73572351b74abfb40f8303e
Rahul Joshi <tj18022826@gmail.com>
2023-03-02 01:11:23 +0530

    SMIL-11 : Consul connect and Dokerfile

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,6 +1,6 @@
+import logging
 from flask import Flask, request
 from flask_cors import CORS
-import json
 import base64
 import cv2
 import numpy as np
@@ -9,26 +9,43 @@ import uuid
 import socket
 
 app = Flask(__name__)
-CORS(app, resources={r"/proc": {"origins": ["http://www.smilebat.xyz", "http://localhost:3000"]}})
-consul_client = consul.Consul(host="38.242.132.44", port=8500)
-service_id = f"proctoring_{str(uuid.uuid4())}"
+CORS(app, resources={r"/*": {"origins": ['www.smilebat.xyz', 'http://localhost:3000'],
+                             "methods": ['GET,PUT,POST,DELETE'],
+                             "allow_headers": ['authorization', 'Content-Type'],
+                             "vary_header": ['Access-Control-Request-Method',
+                                             'Origin',
+                                             'Access-Control-Request-Headers'],
+                             }})
+# consul_client = consul.Consul(host="consul", port=8500)
+consul_client = consul.Consul(host="www.smilebat.xyz", port=8500)
+service_id = f"sb-proc-{str(uuid.uuid4())}"
+logging.basicConfig(level=logging.NOTSET)
+logger = logging.getLogger('sb-proc')
 
 
 def get_free_port():
+    logger.info("Evaluating free ports")
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind(("", 0))
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         return s.getsockname()[1]
 
 
-def register_service_with_consul(port):
-    consul_client.agent.service.register(
-        name="sb-proc",
-        service_id=service_id,
-        address="localhost/proc",
-        port=port,
-        check=consul.Check.http("localhost/health", port, "30s")
-    )
+def register_service_with_consul(host, port):
+    logger.info('Registering to Consul with Hostname : ' + host)
+    try:
+        consul_client.agent.service.register(
+            name="sb-proc",
+            service_id=service_id,
+            port=port,
+            address=host,
+            tags=[],
+            check=consul.Check.http(url=f'http://{host}:{port}/health',
+                                    interval='30s',
+                                    timeout='5s')
+        )
+    except ConnectionError:
+        logger.error('Consul Host is down')
 
 
 def deregister_service_with_consul():
@@ -51,8 +68,12 @@ def api():
 
     if image_list:
         # print(len(image_list))
+        size = len(image_list)
         try:
             for image in image_list:
+                # bs64 = None
+                # if ',' in image:
+                # TODO : Handle empty image
                 bs64 = image.split(',')[1]
                 img = base64.b64decode(bs64);
                 npimg = np.fromstring(img, dtype=np.uint8);
@@ -72,8 +93,8 @@ def api():
                 else:
                     # return 'Alert: No face detected'
                     z += 1
-                if y > 3 or z > 3:
-                    return 'Faulty User\n' + f'good={x}, many={y}, bad={z}'
+                # if y > size/3 or z >3:
+                #     return 'Faulty User\n' + f'good={x}, many={y}, bad={z}'
             return f'good={x}, many={y}, bad={z}'
 
         except Exception as ex:
@@ -82,11 +103,12 @@ def api():
 
 if __name__ == '__main__':
     try:
+        host = socket.gethostname()
         port = get_free_port()
-        print(f"RJ : {port}")
-        register_service_with_consul(port)
-        app.run(port=port, debug=True, use_reloader=False)
+        # port=8153
+        register_service_with_consul(host, port)
+        app.run(host='0.0.0.0', port=port, debug=True, use_reloader=False)
     except Exception as e:
-        print(f"Exception : {e}")
+        logging.error(f"Exception : {e}")
     finally:
         deregister_service_with_consul()
